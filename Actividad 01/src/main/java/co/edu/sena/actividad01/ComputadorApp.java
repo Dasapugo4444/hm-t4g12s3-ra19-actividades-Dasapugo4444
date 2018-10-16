791c680b798eeefade9e07cbf7a02da660e6016b
Santiago <dspulido13@misena.edu.co>
2018-10-16 12:17:56 -0500

    chore: computadorApp corregida

--- Actividad 01/src/main/java/co/edu/sena/actividad01/ComputadorApp.java
+++ Actividad 01/src/main/java/co/edu/sena/actividad01/ComputadorApp.java
@@ -2,7 +2,7 @@ package co.edu.sena.actividad01;
 
 public class ComputadorApp {
     public static void main(String[] args) {
-        
+
     }
 
     public static void escribirComputador(Computador com){
