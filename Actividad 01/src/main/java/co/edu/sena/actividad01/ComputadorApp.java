0a21a837314bf7effe7bcf53415e9c6ea538c737
Santiago <dspulido13@misena.edu.co>
2018-10-17 10:32:41 -0500

    chore: actividad 02 subida

--- Actividad 01/src/main/java/co/edu/sena/actividad01/ComputadorApp.java
+++ Actividad 01/src/main/java/co/edu/sena/actividad01/ComputadorApp.java
@@ -2,9 +2,9 @@ package co.edu.sena.actividad01;
 
 public class ComputadorApp {
     public static void main(String[] args) {
-
     }
 
     public static void escribirComputador(Computador com){
+
     }
 }
